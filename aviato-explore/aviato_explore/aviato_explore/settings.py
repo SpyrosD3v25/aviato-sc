7e58429b4f1d1500c36e2c80cd63314215ee1c2f
SpyrosD3v25 <spyros123go@gmail.com>
2023-05-12 10:55:46 +0300

    fixed staticfiles

--- aviato-explore/aviato_explore/aviato_explore/settings.py
+++ aviato-explore/aviato_explore/aviato_explore/settings.py
@@ -116,10 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, '/static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+# URL prefix for static files
+STATIC_URL = '/static/'
 
 
 # Default primary key field type
